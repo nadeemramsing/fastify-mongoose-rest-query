463d2e014cfb274e55e57419c7de765bcc88a97c
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2021-10-16 14:39:16 +0400

    WIP: Basic structure

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const fastify = require('fastify')
+
+function build(opts = {}) {
+  const app = fastify(opts)
+
+  app.get('/', function (req, rep) {
+    return { hello: 'world' }
+  })
+
+  return app
+}
+
+module.exports = build
---
+++ test/app.test.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const { test } = require('tap')
+const build = require('../app')
+
+test('requests the "/" route', async t => {
+  const app = build()
+
+  t.teardown(() => app.close())
+
+  const response = await app.inject({
+    method: 'GET',
+    url: '/'
+  })
+
+  t.equal(response.statusCode, 200, 'returns a status code of 200')
+})
