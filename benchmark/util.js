71b71632c121319873b5b8b45fdc860ab7433a5d
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2021-10-23 05:51:08 +0400

    Benchmark: Response time per request in ms (avg) added

--- benchmark/util.js
+++ benchmark/util.js
@@ -4,8 +4,20 @@ module.exports = {
 
 function printResult(resultFastify, resultExpress) {
   const table = {
-    'Requests per second (avg)': { 'fastify': resultFastify.requests.average, 'express': resultExpress.requests.average },
-    'Requests per second (max)': { 'fastify': resultFastify.requests.max, 'express': resultExpress.requests.max },
+    'Response time per request in ms (avg)': {
+      'fastify': calcResponseTime(resultFastify),
+      'express': calcResponseTime(resultExpress)
+    },
+
+    'Requests per second (avg)': {
+      'fastify': resultFastify.requests.average,
+      'express': resultExpress.requests.average
+    },
+
+    'Requests per second (max)': {
+      'fastify': resultFastify.requests.max,
+      'express': resultExpress.requests.max
+    },
 
     'Response data throughput per second in Kb (avg)': {
       'fastify': convertByteToKB(resultFastify.throughput.average),
@@ -17,14 +29,32 @@ function printResult(resultFastify, resultExpress) {
       'express': convertByteToKB(resultExpress.throughput.max)
     },
 
-    'Latency (avg)': { 'fastify': resultFastify.latency.average, 'express': resultExpress.latency.average },
-    'Latency (max)': { 'fastify': resultFastify.latency.max, 'express': resultExpress.latency.max },
+    'Latency (avg)': {
+      'fastify': resultFastify.latency.average,
+      'express': resultExpress.latency.average
+    },
 
-    'Number of errors': { 'fastify': resultFastify.errors, 'express': resultExpress.errors },
-    'Number of timeouts': { 'fastify': resultFastify.timeouts, 'express': resultExpress.timeouts }
+    'Latency (max)': {
+      'fastify': resultFastify.latency.max,
+      'express': resultExpress.latency.max
+    },
+
+    'Number of errors': {
+      'fastify': resultFastify.errors,
+      'express': resultExpress.errors
+    },
+
+    'Number of timeouts': {
+      'fastify': resultFastify.timeouts,
+      'express': resultExpress.timeouts
+    }
   }
 
   console.table(table)
+}
+
+function calcResponseTime({ requests, duration }) {
+  return +((duration * 1000) / requests.average).toFixed(2)
 }
 
 function convertByteToKB(n) {
